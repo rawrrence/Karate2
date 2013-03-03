f589256f7a5b07b5585c5ff6863c5be46843007f
Lawrence Tsang <ltsang@andrew.cmu.edu>
2013-03-03 00:04:34 -0500

    changed section validation method

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,16 +1,16 @@
 class Section < ActiveRecord::Base
-  attr_accessible :active, :event_id, :location, :max_age, :max_rank, :min_age, :min_rank
+		attr_accessible :active, :event_id, :location, :max_age, :max_rank, :min_age, :min_rank
 	
 	has_many :registrations
 	has_many :students, :through => :registrations
 	belongs_to :event
-	
-	# validates :max_age, :numericality => { :greater_than_or_equal_to => 5, :greater_than_or_equal_to => :min_age, :only_integer => true }, :allow_blank => true
-	# validates :max_rank, :numericality => { :greater_than_or_equal_to => 1, :greater_than_or_equal_to => :min_rank, :only_integer => true }, :allow_blank => true
-	# validates :min_age, :numericality => { :greater_than_or_equal_to => 5, :less_than_or_equal_to => :max_age, :only_integer => true }
-	# validates :min_rank, :numericality => { :greater_than_or_equal_to => 1, :less_than_or_equal_to => :max_rank, :only_integer => true }
+
+	validates :max_age, :numericality => { :greater_than_or_equal_to => 5, :greater_than_or_equal_to => :min_age, :only_integer => true }, :allow_blank => true
+	validates :max_rank, :numericality => { :greater_than_or_equal_to => 1, :greater_than_or_equal_to => :min_rank, :only_integer => true }, :allow_blank => true
+	validates :min_age, :numericality => { :greater_than_or_equal_to => 5, :less_than_or_equal_to => :max_age, :only_integer => true }
+	validates :min_rank, :numericality => { :greater_than_or_equal_to => 1, :less_than_or_equal_to => :max_rank, :only_integer => true }
 	validates_uniqueness_of :event_id, :scope => [:min_age, :min_rank]
-	validate :event_exists
+	validate :event_active
 
 	
 	scope :active, where('active = ?', true)
@@ -21,7 +21,7 @@ class Section < ActiveRecord::Base
 	scope :for_age, lambda {|age| where('min_age <= ? and max_age >= ?', age, age)}
 	
 	
-	def event_exists
-		return false if Event.find(self.event_id).nil?
+	def event_active
+		return self.event.active
 	end
 end
