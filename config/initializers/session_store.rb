24086878202d5a4b09547d272ab447472dfd98c6
Lawrence Tsang <ltsang@andrew.cmu.edu>
2013-03-02 19:40:59 -0500

    Base app

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Karate2::Application.config.session_store :cookie_store, key: '_Karate2_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Karate2::Application.config.session_store :active_record_store
